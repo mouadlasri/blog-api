9578bcd0b5f8a3608b1df3ab719c22da5abf8c6d
Mouad Lasri <mouadkor20@gmail.com>
2025-08-19 21:18:13 +0900

    Initial project setup

---
+++ src/test/java/org/example/blogapi/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
